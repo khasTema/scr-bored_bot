8b9e04b5ff20df917d3dc2c2a325c833dbfae417
khasTema <akhasin.dev@gmail.com>
2021-11-09 15:12:56 +0200

    https

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 document.querySelector('button').addEventListener('click', function(){
-    fetch('http://www.boredapi.com/api/activity/')
+    fetch('https://www.boredapi.com/api/activity/')
         .then(response => response.json())
         .then(data => {
             document.getElementById('activity').textContent = data.activity
